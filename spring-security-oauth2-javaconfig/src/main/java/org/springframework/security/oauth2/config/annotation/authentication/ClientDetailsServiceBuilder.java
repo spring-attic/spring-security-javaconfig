c69af86ad3715546f20968c1a375cc40296b4836
Rob Winch <rwinch@vmware.com>
2013-05-16 15:22:41 -0500

    Move ClientDetailsServiceBuilder to oauth2 config project

--- spring-security-oauth2-javaconfig/src/main/java/org/springframework/security/oauth2/config/annotation/authentication/ClientDetailsServiceBuilder.java
+++ spring-security-oauth2-javaconfig/src/main/java/org/springframework/security/oauth2/config/annotation/authentication/ClientDetailsServiceBuilder.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.security.oauth.examples.sparklr.config;
+package org.springframework.security.oauth2.config.annotation.authentication;
 
 import java.util.ArrayList;
 import java.util.Collection;
