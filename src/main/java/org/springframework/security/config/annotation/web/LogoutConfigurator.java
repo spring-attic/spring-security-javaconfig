d7a26e5c8e68df96117da472ec88f02624f2e2af
Rob Winch <rwinch@vmware.com>
2013-05-27 10:43:17 -0500

    Polish LogoutConfigurator

--- src/main/java/org/springframework/security/config/annotation/web/LogoutConfigurator.java
+++ src/main/java/org/springframework/security/config/annotation/web/LogoutConfigurator.java
@@ -18,6 +18,9 @@ package org.springframework.security.config.annotation.web;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.security.config.annotation.SecurityConfigurator;
 import org.springframework.security.config.annotation.SecurityConfiguratorAdapter;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.authentication.logout.CookieClearingLogoutHandler;
@@ -27,6 +30,32 @@ import org.springframework.security.web.authentication.logout.LogoutSuccessHandl
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.security.web.authentication.logout.SimpleUrlLogoutSuccessHandler;
 
+/**
+ * Adds logout support. Other {@link SecurityConfigurator} instances may invoke
+ * {@link #addLogoutHandler(LogoutHandler)} in the
+ * {@link #init(HttpConfiguration)} phase.
+ *
+ * <h2>Security Filters</h2>
+ *
+ * The following Filters are populated
+ *
+ * <ul>
+ * <li>
+ * {@link LogoutFilter}</li>
+ * </ul>
+ *
+ * <h2>Shared Objects Created</h2>
+ *
+ * No shared Objects are created
+ *
+ * <h2>Shared Objects Used</h2>
+ *
+ * No shared objects are used.
+ *
+ * @author Rob Winch
+ * @since 3.2
+ * @see RememberMeConfigurator
+ */
 public class LogoutConfigurator extends SecurityConfiguratorAdapter<DefaultSecurityFilterChain,HttpConfiguration> {
     private List<LogoutHandler> logoutHandlers = new ArrayList<LogoutHandler>();
     private SecurityContextLogoutHandler contextLogoutHandler = new SecurityContextLogoutHandler();
@@ -35,49 +64,107 @@ public class LogoutConfigurator extends SecurityConfiguratorAdapter<DefaultSecur
     private String logoutUrl = "/logout";
     private boolean permitAll;
 
-    private LogoutFilter createLogoutFilter() throws Exception {
-        logoutHandlers.add(contextLogoutHandler);
-        LogoutHandler[] handlers = logoutHandlers.toArray(new LogoutHandler[logoutHandlers.size()]);
-        LogoutFilter result = new LogoutFilter(getLogoutSuccessHandler(), handlers);
-        result.setFilterProcessesUrl(logoutUrl);
-        result.afterPropertiesSet();
-        return result;
-    }
-
-    public LogoutConfigurator permitAll() {
-        return permitAll(true);
-    }
-
-    public LogoutConfigurator permitAll(boolean permitAll) {
-        this.permitAll = permitAll;
-        return this;
-    }
-
+    /**
+     * Adds a {@link LogoutHandler}. The {@link SecurityContextLogoutHandler} is
+     * added as the last {@link LogoutHandler} by default.
+     *
+     * @param logoutHandler the {@link LogoutHandler} to add
+     * @return the {@link LogoutConfigurator} for further customization
+     */
     public LogoutConfigurator addLogoutHandler(LogoutHandler logoutHandler) {
         this.logoutHandlers.add(logoutHandler);
         return this;
     }
 
+    /**
+     * Configures {@link SecurityContextLogoutHandler} to invalidate the {@link HttpSession} at the time of logout.
+     * @param invalidateHttpSession true if the {@link HttpSession} should be invalidated (default), or false otherwise.
+     * @return the {@link LogoutConfigurator} for further customization
+     */
     public LogoutConfigurator invalidateHttpSession(boolean invalidateHttpSession) {
         contextLogoutHandler.setInvalidateHttpSession(invalidateHttpSession);
         return this;
     }
 
+    /**
+     * The URL that triggers logout to occur. The default is "/logout"
+     * @param logoutUrl the URL that will invoke logout.
+     * @return the {@link LogoutConfigurator} for further customization
+     */
     public LogoutConfigurator logoutUrl(String logoutUrl) {
         this.logoutUrl = logoutUrl;
         return this;
     }
 
+    /**
+     * The URL to redirect to after logout has occurred. The default is
+     * "/login?logout". This is a shortcut for invoking
+     * {@link #logoutSuccessHandler(LogoutSuccessHandler)} with a
+     * {@link SimpleUrlLogoutSuccessHandler}.
+     *
+     * @param logoutSuccessUrl
+     *            the URL to redirect to after logout occurred
+     * @return the {@link LogoutConfigurator} for further customization
+     */
     public LogoutConfigurator logoutSuccessUrl(String logoutSuccessUrl) {
         this.logoutSuccessUrl = logoutSuccessUrl;
         return this;
     }
 
+    /**
+     * A shortcut for {@link #permitAll(boolean)} with <code>true</code> as an argument.
+     * @return the {@link LogoutConfigurator} for further customizations
+     */
+    public LogoutConfigurator permitAll() {
+        return permitAll(true);
+    }
+
+    /**
+     * Allows specifying the names of cookies to be removed on logout success.
+     * This is a shortcut to easily invoke
+     * {@link #addLogoutHandler(LogoutHandler)} with a
+     * {@link CookieClearingLogoutHandler}.
+     *
+     * @param cookieNamesToClear the names of cookies to be removed on logout success.
+     * @return the {@link LogoutConfigurator} for further customization
+     */
+    public LogoutConfigurator deleteCookies(String... cookieNamesToClear) {
+        return addLogoutHandler(new CookieClearingLogoutHandler(cookieNamesToClear));
+    }
+
+    /**
+     * Sets the {@link LogoutSuccessHandler} to use. If this is specified,
+     * {@link #logoutSuccessUrl(String)} is ignored.
+     *
+     * @param logoutSuccessHandler
+     *            the {@link LogoutSuccessHandler} to use after a user has been
+     *            logged out.
+     * @return the {@link LogoutConfigurator} for further customizations
+     */
     public LogoutConfigurator logoutSuccessHandler(LogoutSuccessHandler logoutSuccessHandler) {
+        this.logoutSuccessUrl = null;
         this.logoutSuccessHandler = logoutSuccessHandler;
         return this;
     }
 
+    /**
+     * Grants access to the {@link #logoutSuccessUrl(String)} and the {@link #logoutUrl(String)} for every user.
+     *
+     * @param permitAll if true grants access, else nothing is done
+     * @return the {@link LogoutConfigurator} for further customization.
+     */
+    public LogoutConfigurator permitAll(boolean permitAll) {
+        this.permitAll = permitAll;
+        return this;
+    }
+
+    /**
+     * Gets the {@link LogoutSuccessHandler} if not null, otherwise creates a
+     * new {@link SimpleUrlLogoutSuccessHandler} using the
+     * {@link #logoutSuccessUrl(String)}.
+     *
+     * @return the {@link LogoutSuccessHandler} to use
+     */
     private LogoutSuccessHandler getLogoutSuccessHandler() {
         if(logoutSuccessHandler != null) {
             return logoutSuccessHandler;
@@ -85,18 +172,6 @@ public class LogoutConfigurator extends SecurityConfiguratorAdapter<DefaultSecur
         SimpleUrlLogoutSuccessHandler logoutSuccessHandler = new SimpleUrlLogoutSuccessHandler();
         logoutSuccessHandler.setDefaultTargetUrl(logoutSuccessUrl);
         return logoutSuccessHandler;
-    }
-
-    public LogoutConfigurator deleteCookies(String... cookieNamesToClear) {
-        return addLogoutHandler(new CookieClearingLogoutHandler(cookieNamesToClear));
-    }
-
-    String getLogoutSuccesUrl() {
-        return logoutSuccessHandler == null ? logoutSuccessUrl : null;
-    }
-
-    String getLogoutUrl() {
-        return logoutUrl;
     }
 
     @Override
@@ -110,5 +185,22 @@ public class LogoutConfigurator extends SecurityConfiguratorAdapter<DefaultSecur
     public void configure(HttpConfiguration http) throws Exception {
         LogoutFilter logoutFilter = createLogoutFilter();
         http.addFilter(logoutFilter);
+    }
+
+    /**
+     * Creates the {@link LogoutFilter} using the {@link LogoutHandler}
+     * instances, the {@link #logoutSuccessHandler(LogoutSuccessHandler)} and
+     * the {@link #logoutUrl(String)}.
+     *
+     * @return the {@link LogoutFilter} to use.
+     * @throws Exception
+     */
+    private LogoutFilter createLogoutFilter() throws Exception {
+        logoutHandlers.add(contextLogoutHandler);
+        LogoutHandler[] handlers = logoutHandlers.toArray(new LogoutHandler[logoutHandlers.size()]);
+        LogoutFilter result = new LogoutFilter(getLogoutSuccessHandler(), handlers);
+        result.setFilterProcessesUrl(logoutUrl);
+        result.afterPropertiesSet();
+        return result;
     }
 }
