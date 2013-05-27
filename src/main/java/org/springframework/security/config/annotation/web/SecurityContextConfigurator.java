2c4686be5a2292c8de9d1e4f501b4b8ae6ccec32
Rob Winch <rwinch@vmware.com>
2013-05-27 16:40:36 -0500

    javadoc SecurityContextConfigurator

--- src/main/java/org/springframework/security/config/annotation/web/SecurityContextConfigurator.java
+++ src/main/java/org/springframework/security/config/annotation/web/SecurityContextConfigurator.java
@@ -16,23 +16,62 @@
 package org.springframework.security.config.annotation.web;
 
 import org.springframework.security.config.annotation.SecurityConfiguratorAdapter;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.context.SecurityContextPersistenceFilter;
 import org.springframework.security.web.context.SecurityContextRepository;
 
 /**
- * @author Rob Winch
+ * Allows persisting and restoring of the {@link SecurityContext} found on the
+ * {@link SecurityContextHolder} for each request by configuring the
+ * {@link SecurityContextPersistenceFilter}. All properties have reasonable
+ * defaults, so no additional configuration is required other than applying this
+ * {@link org.springframework.security.config.annotation.SecurityConfigurator}.
  *
+ * <h2>Security Filters</h2>
+ *
+ * The following Filters are populated
+ *
+ * <ul>
+ * <li>{@link SecurityContextPersistenceFilter}</li>
+ * </ul>
+ *
+ * <h2>Shared Objects Created</h2>
+ *
+ * No shared objects are created.
+ *
+ * <h2>Shared Objects Used</h2>
+ *
+ * The following shared objects are used:
+ *
+ * <ul>
+ * <li>If {@link SessionManagementConfigurator}, is provided and set to always,
+ * then the
+ * {@link SecurityContextPersistenceFilter#setForceEagerSessionCreation(boolean)}
+ * will be set to true.</li>
+ * <li>{@link SecurityContextRepository} must be set and is used on
+ * {@link SecurityContextPersistenceFilter}. This is typically configured using
+ * {@link HttpConfiguration#securityContextRepsitory(SecurityContextRepository)}.</li>
+ * </ul>
+ *
+ * @author Rob Winch
+ * @since 3.2
  */
-public class SecurityContextConfigurator extends SecurityConfiguratorAdapter<DefaultSecurityFilterChain,HttpConfiguration> {
+public class SecurityContextConfigurator extends
+        SecurityConfiguratorAdapter<DefaultSecurityFilterChain, HttpConfiguration> {
 
     @Override
     public void configure(HttpConfiguration http) throws Exception {
 
-        SecurityContextPersistenceFilter securityContextFilter = new SecurityContextPersistenceFilter(http.getSharedObject(SecurityContextRepository.class));
-        SessionManagementConfigurator sessionManagement = http.getConfigurator(SessionManagementConfigurator.class);
-        SessionCreationPolicy sessionCreationPolicy = sessionManagement.sessionCreationPolicy();
-        if(SessionCreationPolicy.always == sessionCreationPolicy) {
+        SecurityContextRepository securityContextRepository = http.getSharedObject(SecurityContextRepository.class);
+        SecurityContextPersistenceFilter securityContextFilter = new SecurityContextPersistenceFilter(
+                securityContextRepository);
+        SessionManagementConfigurator sessionManagement = http
+                .getConfigurator(SessionManagementConfigurator.class);
+        SessionCreationPolicy sessionCreationPolicy = sessionManagement == null ? null
+                : sessionManagement.sessionCreationPolicy();
+        if (SessionCreationPolicy.always == sessionCreationPolicy) {
             securityContextFilter.setForceEagerSessionCreation(true);
         }
         securityContextFilter.afterPropertiesSet();
