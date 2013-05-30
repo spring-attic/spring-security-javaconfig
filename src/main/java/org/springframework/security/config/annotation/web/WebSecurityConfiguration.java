808b0f60da1de067dbc745edb7bee1498b450691
Rob Winch <rwinch@vmware.com>
2013-05-30 09:58:05 -0500

    javadoc WebSecurityConfiguration

--- src/main/java/org/springframework/security/config/annotation/web/WebSecurityConfiguration.java
+++ src/main/java/org/springframework/security/config/annotation/web/WebSecurityConfiguration.java
@@ -15,7 +15,6 @@
  */
 package org.springframework.security.config.annotation.web;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -36,13 +35,23 @@ import org.springframework.security.web.access.expression.DefaultWebSecurityExpr
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 
 /**
+ * Uses a {@link WebSecurityBuilder} to create the {@link FilterChainProxy} that
+ * performs the web based security for Spring Security. It then exports the
+ * necessary beans. Customizations can be made to {@link WebSecurityBuilder} by
+ * extending {@link WebSecurityConfigurerAdapter} and exposing it as a
+ * {@link Configuration} or implementing {@link WebSecurityConfigurer} and
+ * exposing it as a {@link Configuration}. This configuration is imported when
+ * using {@link EnableWebSecurity}.
+ *
+ * @see EnableWebSecurity
+ * @see WebSecurityBuilder
  *
  * @author Rob Winch
  * @since 3.2
  */
 @Configuration
 public class WebSecurityConfiguration {
-    private final WebSecurityBuilder springSecurityFilterChain = new WebSecurityBuilder();
+    private final WebSecurityBuilder webSecurityBuilder = new WebSecurityBuilder();
 
     private List<SecurityConfigurator<FilterChainProxy, WebSecurityBuilder>> webSecurityConfigurers;
 
@@ -51,25 +60,41 @@ public class WebSecurityConfiguration {
         return new DefaultWebSecurityExpressionHandler();
     }
 
+    /**
+     * Creates the Spring Security Filter Chain
+     * @return
+     * @throws Exception
+     */
     @Bean(name="springSecurityFilterChain")
     public FilterChainProxy springSecurityFilterChain() throws Exception {
         boolean hasConfigurators = webSecurityConfigurers != null && !webSecurityConfigurers.isEmpty();
         if(!hasConfigurators) {
             throw new IllegalStateException("At least one non-null instance of "+ WebSecurityConfigurer.class.getSimpleName()+" must be exposed as a @Bean when using @EnableWebSecurity. Hint try extending "+ WebSecurityConfigurerAdapter.class.getSimpleName());
         }
-        return springSecurityFilterChain.build();
+        return webSecurityBuilder.build();
     }
 
+    /**
+     * Creates the {@link WebInvocationPrivilegeEvaluator} that is necessary for the JSP tag support.
+     * @return the {@link WebInvocationPrivilegeEvaluator}
+     * @throws Exception
+     */
     @Bean
     public WebInvocationPrivilegeEvaluator privilegeEvaluator() throws Exception {
-        FilterSecurityInterceptor securityInterceptor = springSecurityFilterChain.getSecurityInterceptor();
+        FilterSecurityInterceptor securityInterceptor = webSecurityBuilder.getSecurityInterceptor();
         return securityInterceptor == null ? null : new DefaultWebInvocationPrivilegeEvaluator(securityInterceptor);
     }
 
+    /**
+     * Sets the {@code <SecurityConfigurator<FilterChainProxy, WebSecurityBuilder>} instances used to create the web configuration.
+     *
+     * @param webSecurityConfigurers the {@code <SecurityConfigurator<FilterChainProxy, WebSecurityBuilder>} instances used to create the web configuration
+     * @throws Exception
+     */
     @Autowired(required = false)
     public void setFilterChainProxySecurityConfigurator(
             List<SecurityConfigurator<FilterChainProxy, WebSecurityBuilder>> webSecurityConfigurers) throws Exception {
-        Collections.sort(webSecurityConfigurers, new AnnotationAwareOrderComparator());
+        Collections.sort(webSecurityConfigurers, AnnotationAwareOrderComparator.INSTANCE);
 
         Integer previousOrder = null;
         for(SecurityConfigurator<FilterChainProxy, WebSecurityBuilder> config : webSecurityConfigurers) {
@@ -80,13 +105,22 @@ public class WebSecurityConfiguration {
             previousOrder = order;
         }
         for(SecurityConfigurator<FilterChainProxy, WebSecurityBuilder> webSecurityConfigurer : webSecurityConfigurers) {
-            springSecurityFilterChain.apply(webSecurityConfigurer);
+            webSecurityBuilder.apply(webSecurityConfigurer);
         }
         this.webSecurityConfigurers = webSecurityConfigurers;
     }
 
 
+    /**
+     * A custom verision of the Spring provided AnnotationAwareOrderComparator
+     * that uses {@link AnnotationUtils#findAnnotation(Class, Class)} to look on
+     * super class instances for the {@link Order} annotation.
+     *
+     * @author Rob Winch
+     * @since 3.2
+     */
     private static class AnnotationAwareOrderComparator extends OrderComparator {
+        private static AnnotationAwareOrderComparator INSTANCE = new AnnotationAwareOrderComparator();
 
         @Override
         protected int getOrder(Object obj) {
@@ -107,32 +141,5 @@ public class WebSecurityConfiguration {
             }
             return Ordered.LOWEST_PRECEDENCE;
         }
-
-        /**
-         * Sort the given List with a default AnnotationAwareOrderComparator.
-         * <p>Optimized to skip sorting for lists with size 0 or 1,
-         * in order to avoid unnecessary array extraction.
-         * @param list the List to sort
-         * @see java.util.Collections#sort(java.util.List, java.util.Comparator)
-         */
-        public static void sort(List<?> list) {
-            if (list.size() > 1) {
-                Collections.sort(list, INSTANCE);
-            }
-        }
-
-        /**
-         * Sort the given array with a default AnnotationAwareOrderComparator.
-         * <p>Optimized to skip sorting for lists with size 0 or 1,
-         * in order to avoid unnecessary array extraction.
-         * @param array the array to sort
-         * @see java.util.Arrays#sort(Object[], java.util.Comparator)
-         */
-        public static void sort(Object[] array) {
-            if (array.length > 1) {
-                Arrays.sort(array, INSTANCE);
-            }
-        }
-
     }
 }
