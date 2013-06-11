0cfccd8a885cbe9297ede2cfa5d020c3ff996262
Rob Winch <rwinch@vmware.com>
2013-06-11 16:03:35 -0500

    format NamespaceHttpTests

--- src/test/groovy/org/springframework/security/config/annotation/web/NamespaceHttpTests.groovy
+++ src/test/groovy/org/springframework/security/config/annotation/web/NamespaceHttpTests.groovy
@@ -56,13 +56,13 @@ import org.springframework.security.web.util.RequestMatcher
 public class NamespaceHttpTests extends BaseSpringSpec {
     def "http@access-decision-manager-ref"() {
         setup:
-        AccessDecisionManagerRefConfig.ACCESS_DECISION_MGR = Mock(AccessDecisionManager)
-        AccessDecisionManagerRefConfig.ACCESS_DECISION_MGR.supports(FilterInvocation) >> true
-        AccessDecisionManagerRefConfig.ACCESS_DECISION_MGR.supports(_ as ConfigAttribute) >> true
+            AccessDecisionManagerRefConfig.ACCESS_DECISION_MGR = Mock(AccessDecisionManager)
+            AccessDecisionManagerRefConfig.ACCESS_DECISION_MGR.supports(FilterInvocation) >> true
+            AccessDecisionManagerRefConfig.ACCESS_DECISION_MGR.supports(_ as ConfigAttribute) >> true
         when:
-        loadConfig(AccessDecisionManagerRefConfig)
+            loadConfig(AccessDecisionManagerRefConfig)
         then:
-        findFilter(FilterSecurityInterceptor).accessDecisionManager == AccessDecisionManagerRefConfig.ACCESS_DECISION_MGR
+            findFilter(FilterSecurityInterceptor).accessDecisionManager == AccessDecisionManagerRefConfig.ACCESS_DECISION_MGR
     }
 
     @Configuration
@@ -79,9 +79,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@access-denied-page"() {
         when:
-        loadConfig(AccessDeniedPageConfig)
+            loadConfig(AccessDeniedPageConfig)
         then:
-        findFilter(ExceptionTranslationFilter).accessDeniedHandler.errorPage == "/AccessDeniedPageConfig"
+            findFilter(ExceptionTranslationFilter).accessDeniedHandler.errorPage == "/AccessDeniedPageConfig"
     }
 
     @Configuration
@@ -95,9 +95,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@authentication-manager-ref"() {
         when: "Specify AuthenticationManager"
-        loadConfig(AuthenticationManagerRefConfig)
+            loadConfig(AuthenticationManagerRefConfig)
         then: "Populates the AuthenticationManager"
-        findFilter(FilterSecurityInterceptor).authenticationManager.parent.class == CustomAuthenticationManager
+            findFilter(FilterSecurityInterceptor).authenticationManager.parent.class == CustomAuthenticationManager
     }
 
     @Configuration
@@ -128,12 +128,12 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@create-session=always"() {
         when:
-        loadConfig(IfRequiredConfig)
+            loadConfig(IfRequiredConfig)
         then:
-        findFilter(SecurityContextPersistenceFilter).forceEagerSessionCreation == false
-        findFilter(SecurityContextPersistenceFilter).repo.allowSessionCreation == true
-        findFilter(SessionManagementFilter).securityContextRepository.allowSessionCreation == true
-        findFilter(ExceptionTranslationFilter).requestCache.class == HttpSessionRequestCache
+            findFilter(SecurityContextPersistenceFilter).forceEagerSessionCreation == false
+            findFilter(SecurityContextPersistenceFilter).repo.allowSessionCreation == true
+            findFilter(SessionManagementFilter).securityContextRepository.allowSessionCreation == true
+            findFilter(ExceptionTranslationFilter).requestCache.class == HttpSessionRequestCache
     }
 
     @Configuration
@@ -147,13 +147,13 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@create-session=stateless"() {
         when:
-        loadConfig(CreateSessionStatelessConfig)
+            loadConfig(CreateSessionStatelessConfig)
         then:
-        findFilter(SecurityContextPersistenceFilter).forceEagerSessionCreation == false
-        findFilter(SecurityContextPersistenceFilter).repo.class == NullSecurityContextRepository
-        findFilter(SessionManagementFilter).securityContextRepository.class == NullSecurityContextRepository
-        findFilter(ExceptionTranslationFilter).requestCache.class == NullRequestCache
-        findFilter(RequestCacheAwareFilter).requestCache.class == NullRequestCache
+            findFilter(SecurityContextPersistenceFilter).forceEagerSessionCreation == false
+            findFilter(SecurityContextPersistenceFilter).repo.class == NullSecurityContextRepository
+            findFilter(SessionManagementFilter).securityContextRepository.class == NullSecurityContextRepository
+            findFilter(ExceptionTranslationFilter).requestCache.class == NullRequestCache
+            findFilter(RequestCacheAwareFilter).requestCache.class == NullRequestCache
     }
 
     @Configuration
@@ -167,11 +167,11 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@create-session=ifRequired"() {
         when:
-        loadConfig(IfRequiredConfig)
+            loadConfig(IfRequiredConfig)
         then:
-        findFilter(SecurityContextPersistenceFilter).forceEagerSessionCreation == false
-        findFilter(SecurityContextPersistenceFilter).repo.allowSessionCreation == true
-        findFilter(SessionManagementFilter).securityContextRepository.allowSessionCreation == true
+            findFilter(SecurityContextPersistenceFilter).forceEagerSessionCreation == false
+            findFilter(SecurityContextPersistenceFilter).repo.allowSessionCreation == true
+            findFilter(SessionManagementFilter).securityContextRepository.allowSessionCreation == true
     }
 
     @Configuration
@@ -185,20 +185,20 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@create-session defaults to ifRequired"() {
         when:
-        loadConfig(IfRequiredConfig)
+            loadConfig(IfRequiredConfig)
         then:
-        findFilter(SecurityContextPersistenceFilter).forceEagerSessionCreation == false
-        findFilter(SecurityContextPersistenceFilter).repo.allowSessionCreation == true
-        findFilter(SessionManagementFilter).securityContextRepository.allowSessionCreation == true
+            findFilter(SecurityContextPersistenceFilter).forceEagerSessionCreation == false
+            findFilter(SecurityContextPersistenceFilter).repo.allowSessionCreation == true
+            findFilter(SessionManagementFilter).securityContextRepository.allowSessionCreation == true
     }
 
     def "http@create-session=never"() {
         when:
-        loadConfig(CreateSessionNeverConfig)
+            loadConfig(CreateSessionNeverConfig)
         then:
-        findFilter(SecurityContextPersistenceFilter).forceEagerSessionCreation == false
-        findFilter(SecurityContextPersistenceFilter).repo.allowSessionCreation == false
-        findFilter(SessionManagementFilter).securityContextRepository.allowSessionCreation == false
+            findFilter(SecurityContextPersistenceFilter).forceEagerSessionCreation == false
+            findFilter(SecurityContextPersistenceFilter).repo.allowSessionCreation == false
+            findFilter(SessionManagementFilter).securityContextRepository.allowSessionCreation == false
     }
 
     @Configuration
@@ -218,9 +218,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@disable-url-rewriting = true (default for Java Config)"() {
         when:
-        loadConfig(DefaultUrlRewritingConfig)
+            loadConfig(DefaultUrlRewritingConfig)
         then:
-        findFilter(SecurityContextPersistenceFilter).repo.disableUrlRewriting
+            findFilter(SecurityContextPersistenceFilter).repo.disableUrlRewriting
     }
 
     @Configuration
@@ -233,9 +233,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@disable-url-rewriting = false"() {
         when:
-        loadConfig(EnableUrlRewritingConfig)
+            loadConfig(EnableUrlRewritingConfig)
         then:
-        findFilter(SecurityContextPersistenceFilter).repo.disableUrlRewriting == false
+            findFilter(SecurityContextPersistenceFilter).repo.disableUrlRewriting == false
     }
 
     @Configuration
@@ -252,9 +252,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@entry-point-ref"() {
         when:
-        loadConfig(EntryPointRefConfig)
+            loadConfig(EntryPointRefConfig)
         then:
-        findFilter(ExceptionTranslationFilter).authenticationEntryPoint.loginFormUrl == "/EntryPointRefConfig"
+            findFilter(ExceptionTranslationFilter).authenticationEntryPoint.loginFormUrl == "/EntryPointRefConfig"
     }
 
     @Configuration
@@ -267,9 +267,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@jaas-api-provision"() {
         when:
-        loadConfig(JaasApiProvisionConfig)
+            loadConfig(JaasApiProvisionConfig)
         then:
-        findFilter(JaasApiIntegrationFilter)
+            findFilter(JaasApiIntegrationFilter)
     }
 
     @Configuration
@@ -284,9 +284,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@once-per-request=true"() {
         when:
-        loadConfig(OncePerRequestConfig)
+            loadConfig(OncePerRequestConfig)
         then:
-        findFilter(FilterSecurityInterceptor).observeOncePerRequest
+            findFilter(FilterSecurityInterceptor).observeOncePerRequest
     }
 
     @Configuration
@@ -301,9 +301,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@once-per-request=false"() {
         when:
-        loadConfig(OncePerRequestFalseConfig)
+            loadConfig(OncePerRequestFalseConfig)
         then:
-        !findFilter(FilterSecurityInterceptor).observeOncePerRequest
+            !findFilter(FilterSecurityInterceptor).observeOncePerRequest
     }
 
     @Configuration
@@ -325,9 +325,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@realm"() {
         when:
-        loadConfig(RealmConfig)
+            loadConfig(RealmConfig)
         then:
-        findFilter(BasicAuthenticationFilter).authenticationEntryPoint.realmName == "RealmConfig"
+            findFilter(BasicAuthenticationFilter).authenticationEntryPoint.realmName == "RealmConfig"
     }
 
     @Configuration
@@ -342,9 +342,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@request-matcher-ref ant"() {
         when:
-        loadConfig(RequestMatcherAntConfig)
+            loadConfig(RequestMatcherAntConfig)
         then:
-        filterChain(0).requestMatcher.pattern == "/api/**"
+            filterChain(0).requestMatcher.pattern == "/api/**"
     }
 
     @Configuration
@@ -357,12 +357,12 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@request-matcher-ref regex"() {
         when:
-        loadConfig(RequestMatcherRegexConfig)
+            loadConfig(RequestMatcherRegexConfig)
         then:
-        filterChain(0).requestMatcher.class == RegexRequestMatcher
-        filterChain(0).requestMatcher.pattern.matcher("/regex/a")
-        filterChain(0).requestMatcher.pattern.matcher("/regex/b")
-        !filterChain(0).requestMatcher.pattern.matcher("/regex1/b")
+            filterChain(0).requestMatcher.class == RegexRequestMatcher
+            filterChain(0).requestMatcher.pattern.matcher("/regex/a")
+            filterChain(0).requestMatcher.pattern.matcher("/regex/b")
+            !filterChain(0).requestMatcher.pattern.matcher("/regex1/b")
     }
 
     @Configuration
@@ -375,9 +375,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@request-matcher-ref"() {
         when:
-        loadConfig(RequestMatcherRefConfig)
+            loadConfig(RequestMatcherRefConfig)
         then:
-        filterChain(0).requestMatcher.class == MyRequestMatcher
+            filterChain(0).requestMatcher.class == MyRequestMatcher
     }
 
     @Configuration
@@ -395,12 +395,12 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@security=none"() {
         when:
-        loadConfig(SecurityNoneConfig)
+            loadConfig(SecurityNoneConfig)
         then:
-        filterChain(0).requestMatcher.pattern == "/resources/**"
-        filterChain(0).filters.empty
-        filterChain(1).requestMatcher.pattern == "/public/**"
-        filterChain(1).filters.empty
+            filterChain(0).requestMatcher.pattern == "/resources/**"
+            filterChain(0).filters.empty
+            filterChain(1).requestMatcher.pattern == "/public/**"
+            filterChain(1).filters.empty
     }
 
     @Configuration
@@ -421,9 +421,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@security-context-repository-ref"() {
         when:
-        loadConfig(SecurityContextRepoConfig)
+            loadConfig(SecurityContextRepoConfig)
         then:
-        findFilter(SecurityContextPersistenceFilter).repo.class == NullSecurityContextRepository
+            findFilter(SecurityContextPersistenceFilter).repo.class == NullSecurityContextRepository
     }
 
     @Configuration
@@ -437,9 +437,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@servlet-api-provision=false"() {
         when:
-        loadConfig(ServletApiProvisionConfig)
+            loadConfig(ServletApiProvisionConfig)
         then:
-        findFilter(SecurityContextHolderAwareRequestFilter) == null
+            findFilter(SecurityContextHolderAwareRequestFilter) == null
     }
 
     @Configuration
@@ -451,9 +451,9 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@servlet-api-provision defaults to true"() {
         when:
-        loadConfig(ServletApiProvisionDefaultsConfig)
+            loadConfig(ServletApiProvisionDefaultsConfig)
         then:
-        findFilter(SecurityContextHolderAwareRequestFilter) != null
+            findFilter(SecurityContextHolderAwareRequestFilter) != null
     }
 
     @Configuration
@@ -464,10 +464,10 @@ public class NamespaceHttpTests extends BaseSpringSpec {
 
     def "http@use-expressions=false"() {
         when:
-        loadConfig(DisableUseExpressionsConfig)
+            loadConfig(DisableUseExpressionsConfig)
         then:
-        findFilter(FilterSecurityInterceptor).securityMetadataSource.class == DefaultFilterInvocationSecurityMetadataSource
-        findFilter(FilterSecurityInterceptor).accessDecisionManager.decisionVoters.collect { it.class } == [RoleVoter, AuthenticatedVoter]
+            findFilter(FilterSecurityInterceptor).securityMetadataSource.class == DefaultFilterInvocationSecurityMetadataSource
+            findFilter(FilterSecurityInterceptor).accessDecisionManager.decisionVoters.collect { it.class } == [RoleVoter, AuthenticatedVoter]
     }
 
     @Configuration
