87115e61da2986ad4a94c60678a0fb4d0671bb1a
Rob Winch <rwinch@vmware.com>
2013-06-11 16:41:41 -0500

    Format ExpressionUrlAuthorizationTests

--- src/test/groovy/org/springframework/security/config/annotation/web/ExpressionUrlAuthorizationsTests.groovy
+++ src/test/groovy/org/springframework/security/config/annotation/web/ExpressionUrlAuthorizationsTests.groovy
@@ -23,23 +23,23 @@ public class ExpressionUrlAuthorizationsTests extends Specification {
 
     def "hasAnyAuthority('ROLE_USER')"() {
         when:
-        def expression = ExpressionUrlAuthorizations.hasAnyAuthority("ROLE_USER")
+            def expression = ExpressionUrlAuthorizations.hasAnyAuthority("ROLE_USER")
         then:
-        expression == "hasAnyAuthority('ROLE_USER')"
+            expression == "hasAnyAuthority('ROLE_USER')"
     }
 
     def "hasAnyAuthority('ROLE_USER','ROLE_ADMIN')"() {
         when:
-        def expression = ExpressionUrlAuthorizations.hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
+            def expression = ExpressionUrlAuthorizations.hasAnyAuthority("ROLE_USER","ROLE_ADMIN")
         then:
-        expression == "hasAnyAuthority('ROLE_USER','ROLE_ADMIN')"
+            expression == "hasAnyAuthority('ROLE_USER','ROLE_ADMIN')"
     }
 
     def "hasRole('ROLE_USER') is rejected due to starting with ROLE_"() {
         when:
-        def expression = ExpressionUrlAuthorizations.hasRole("ROLE_USER")
+            def expression = ExpressionUrlAuthorizations.hasRole("ROLE_USER")
         then:
-        IllegalArgumentException e = thrown()
-        e.message == "role should not start with 'ROLE_' since it is automatically inserted. Got 'ROLE_USER'"
+            IllegalArgumentException e = thrown()
+            e.message == "role should not start with 'ROLE_' since it is automatically inserted. Got 'ROLE_USER'"
     }
 }
