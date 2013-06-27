fc28615e443e37dc5fda40b2705bf6880d191046
Rob Winch <rwinch@vmware.com>
2013-06-27 14:23:08 -0500

    Remove unnecessary code from Hello World SecurityConfig

--- samples/helloworld/src/main/java/org/springframework/security/samples/config/SecurityConfig.java
+++ samples/helloworld/src/main/java/org/springframework/security/samples/config/SecurityConfig.java
@@ -2,19 +2,12 @@ package org.springframework.security.samples.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-        web
-            .ignoring().antMatchers("");
-    }
 
     @Override
     protected void registerAuthentication(AuthenticationManagerBuilder auth) throws Exception {
